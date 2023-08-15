e59eac440c616747ba90791be6197839165766a1
Akindele Ebenezer <tobi.akindele@gmail.com>
2023-08-15 20:55:24 +0100

    PRODUCTION MODE

--- resources/views/Components/Datalist/VehicleListComponent.blade.php
+++ resources/views/Components/Datalist/VehicleListComponent.blade.php
@@ -3,20 +3,21 @@
     @foreach ($Cars_Absolute as $Car)
         @if (empty($Car->VehicleNumber)) @continue @endif 
         @php
-            $CarStatus = \DB::table('cars')->select('Status')->where('VehicleNumber', $Car->VehicleNumber)->first();
+            $Car__DATA = \DB::table('cars')->select(['Status', 'Driver', 'Balance'])->where('VehicleNumber', $Car->VehicleNumber)->first();
+            // $CarStatus = \DB::table('cars')->select('Status')->where('VehicleNumber', $Car->VehicleNumber)->first();
             $CarMileage = \DB::table('refuelings')->select('Mileage')->where('VehicleNumber', $Car->VehicleNumber)->orderBy('Date', 'DESC')->first(); 
-            $CarDriver = \DB::table('cars')->select('Driver')->where('VehicleNumber', $Car->VehicleNumber)->first(); 
-            $CarBalance = \DB::table('cars')->select('Balance')->where('VehicleNumber', $Car->VehicleNumber)->first(); 
+            // $CarDriver = \DB::table('cars')->select('Driver')->where('VehicleNumber', $Car->VehicleNumber)->first(); 
+            // $CarBalance = \DB::table('cars')->select('Balance')->where('VehicleNumber', $Car->VehicleNumber)->first(); 
         @endphp 
         <div class="data-values">
             <span>{{ $Car->VehicleNumber }}</span>
             <span class="Hide CarCardNumber">{{ $Car->CardNumber }}</span>
-            <span class="{{ $CarStatus->Status === 'ACTIVE' ? 'active-x' : 'inactive-x' }}"></span>
+            <span class="{{ $Car__DATA->Status === 'ACTIVE' ? 'active-x' : 'inactive-x' }}"></span>
             <span class="Hide"></span>
             <span>{{ $Car->Maker }} <br> {{ $Car->CardNumber }}</span>
             <span class="Hide">{{ $CarMileage->Mileage ?? 0 }}</span>
-            <span class="Hide">{{ $CarDriver->Driver ?? 'the car driver' }}</span>
-            <span class="Hide">{{ $CarBalance->Balance ?? 0 }}</span> 
+            <span class="Hide">{{ $Car__DATA->Driver ?? 'the car driver' }}</span>
+            <span class="Hide">{{ $Car__DATA->Balance ?? 0 }}</span> 
         </div>
     @endforeach
 </div>  
