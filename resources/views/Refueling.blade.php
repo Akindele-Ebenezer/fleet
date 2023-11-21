800f4d83bc120962f03e3e68a7011c1e8e3574d1
Akindele Ebenezer <tobi.akindele@gmail.com>
2023-11-21 19:09:08 +0100

    PRODUCTION MODE

--- resources/views/Refueling.blade.php
+++ resources/views/Refueling.blade.php
@@ -202,11 +202,14 @@
                             {{ round($Refueling->Consumption, 1) }} <small>{{ $CarOdometer->Odometer === 'Kilometer' ? 'km/liter' : '' }}{{ $CarOdometer->Odometer === 'Mileage' ? 'mpl' : '' }}</em>
                         </option>
                         <option> 
-                            {{ $CarOdometer->Odometer === 'Mileage' ? round($Refueling->KM * 0.621371 / $Refueling->Quantity, 1) . ' km/liter' : '' }}
-                            {{ $CarOdometer->Odometer === 'Kilometer' ? round($Refueling->KM * 0.621371 / $Refueling->Quantity, 1) . ' km/liter' : '' }}
-                            {{-- @if ($CarOdometer->Odometer === 'Mileage')
-                                {{ round($Refueling->KM * 0.621371 / $Refueling->Quantity, 1) . ' km/liter' }}
-                            @endif --}}
+                            {{-- {{ $CarOdometer->Odometer === 'Kilometer' ? round($Refueling->KM * 0.621371 / $Refueling->Quantity, 1) . ' km/liter' : '' }}
+                            {{ $CarOdometer->Odometer === 'Mileage' ? round($Refueling->KM * 0.621371 / $Refueling->Quantity, 1) . ' km/liter' : '' }} --}}
+                            @if ($CarOdometer->Odometer === 'Mileage') 
+                                {{ round($Refueling->KM * 1.60934 / $Refueling->Quantity, 1) . ' km/liter' }}
+                            @endif
+                            @if ($CarOdometer->Odometer === 'Kilometer') 
+                                {{ round($Refueling->KM * 0.621371 / $Refueling->Quantity, 1) . ' mpl' }}
+                            @endif
                         </option>
                     </select> 
                 </td>
