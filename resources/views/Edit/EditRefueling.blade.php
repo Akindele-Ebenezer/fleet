75a5e1fabdc4082ec4aef8bbe60f108963498934
Akindele Ebenezer <tobi.akindele@gmail.com>
2023-11-21 15:57:46 +0100

    PRODUCTION MODE

--- resources/views/Edit/EditRefueling.blade.php
+++ resources/views/Edit/EditRefueling.blade.php
@@ -51,9 +51,21 @@
                 <td>{{ $Refueling->Quantity }}</td>
                 <td>₦ {{ empty($Refueling->Amount) ? '' : number_format($Refueling->Amount) }}</td>
                 <td>{{ $Refueling->ReceiptNumber }}</td> 
-                <td>{{ $Refueling->KM }} {{ $CarOdometer->Odometer === 'Mileage' ? 'miles' : '' }}{{ $CarOdometer->Odometer === 'Kilometer' ? 'km' : '' }}</td> 
+                <td>{{ $Refueling->KM }}
+                    @if (empty($CarOdometer->Odometer))
+                        null 
+                        @else 
+                        {{ $CarOdometer->Odometer === 'Mileage' ? 'miles' : '' }}{{ $CarOdometer->Odometer === 'Kilometer' ? 'km' : '' }}
+                    @endif
+                    </td> 
                 <td class="Hide">{{ $Refueling->id }}</td>
-                <td class="fuel-consumption">{{ round($Refueling->Consumption, 1) }} <small>{{ $CarOdometer->Odometer === 'Kilometer' ? 'km/liter' : '' }}{{ $CarOdometer->Odometer === 'Mileage' ? 'mpl' : '' }}</em></td>
+                <td class="fuel-consumption">{{ round($Refueling->Consumption, 1) }}
+                    @if (empty($CarOdometer->Odometer))
+                        null 
+                        @else
+                        <small>{{ $CarOdometer->Odometer === 'Kilometer' ? 'km/liter' : '' }}{{ $CarOdometer->Odometer === 'Mileage' ? 'mpl' : '' }}</em>
+                    @endif
+                </td>
             </tr> 
             @endforeach  
             <div class="table-head filter"> 
