5170bbbaafdf93c9d1b6e725209cf0b76c070130
Akindele Ebenezer <tobi.akindele@gmail.com>
2025-04-24 10:57:40 +0100

    ALERTS & NOTIFICATIONS

--- app/Exports/RefuelingExport.php
+++ app/Exports/RefuelingExport.php
@@ -121,7 +121,6 @@ class RefuelingExport implements
             'I' => 0,             
             'J' => 0,             
             'K' => 0,               
-            'L' => 0,             
             'N' => 0,             
             'O' => 0,            
             'P' => 0,            
