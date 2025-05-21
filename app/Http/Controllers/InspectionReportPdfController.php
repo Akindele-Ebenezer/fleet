00452200cc11e6ac152fc277b42be3025129b481
Akindele Ebenezer <tobi.akindele@gmail.com>
2025-05-21 09:20:17 +0100

    ALERTS & NOTIFICATIONS

--- app/Http/Controllers/InspectionReportPdfController.php
+++ app/Http/Controllers/InspectionReportPdfController.php
@@ -316,7 +316,7 @@ class InspectionReportPdfController extends Controller
         $this->fpdf->Cell(30, 7, '', 0, 0, 'B'); 
         // $this->fpdf->Cell(20, 7, $MyInspectionReport->DateInspected, 0, 0, 'C'); 
         $this->fpdf->SetFont('Arial', '', 11);
-        $this->fpdf->Image('../public/Images/transport-manager-signature.png', 10, 172, 50, 33);
+        // $this->fpdf->Image('../public/Images/transport-manager-signature.png', 10, 172, 50, 33);
         $this->fpdf->Image('../public/Images/fleet-supervisor-signature.png', 169, 185, 29, 19);
         // $this->fpdf->Cell(10, 7, '............................................', 0, 0, 'B');   
         // $this->fpdf->Cell(145, 7, '', 0, 0, 'B'); 
@@ -646,7 +646,7 @@ class InspectionReportPdfController extends Controller
         $this->fpdf->Cell(30, 7, '', 0, 0, 'B'); 
         $this->fpdf->Cell(20, 7, date('Y-m-d'), 0, 0, 'C'); 
         $this->fpdf->SetFont('Arial', '', 11);
-        $this->fpdf->Image('../public/Images/transport-manager-signature.png', 10, 207, 50, 33);
+        // $this->fpdf->Image('../public/Images/transport-manager-signature.png', 10, 207, 50, 33);
         $this->fpdf->Image('../public/Images/fleet-supervisor-signature.png', 169, 220, 29, 19);
         // $this->fpdf->Cell(10, 7, '............................................', 0, 0, 'B');   
         // $this->fpdf->Cell(145, 7, '', 0, 0, 'B'); 
